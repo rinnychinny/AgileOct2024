88f747e900826c9caab076f8e959173cebdab932
Hesperus <tmw19@student.london.ac.uk>
2025-02-11 15:26:58 +0200

    Set up site structure
    Uses filing system to direct to pages on site

--- ai-study-tool/src/app/login/page.tsx
+++ ai-study-tool/src/app/login/page.tsx
@@ -4,25 +4,12 @@ export default function LogIn() {
   return (
     
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:not-invert"
-          src="/logo.svg"
-          alt="App logo"
-          width={180}
-          height={38}
-          priority
-        />
-        </main>
-
-
-        <div className="flex gap- items-center flex-col sm:flex-row">
+      <header>
+        <div className="flex gap-10 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/home"
+            //rel="noopener noreferrer"
           >
             <Image
               className="dark:not-invert"
@@ -35,13 +22,25 @@ export default function LogIn() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="/account"
           >
-            Log in
+            Account
           </a>
         </div>
+</header>
+      
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:not-invert"
+          src="/logo.svg"
+          alt="App logo"
+          width={600}
+          height={38}
+          priority
+        />
+        </main>
+
+
       
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
--- ai-study-tool/src/app/summarize/page.tsx
+++ ai-study-tool/src/app/summarize/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Landing() {
+export default function summarize() {
   return (
     
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -9,6 +9,7 @@ export default function Landing() {
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/home"
+            //rel="noopener noreferrer"
           >
             <Image
               className="dark:not-invert"
@@ -20,10 +21,38 @@ export default function Landing() {
             Home Page
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="login"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/pastpaper"
+            //rel="noopener noreferrer"
           >
-            Log in
+            <Image
+              className="dark:not-invert"
+              src="/logo.svg"
+              alt="App logo"
+              width={20}
+              height={20}
+            />
+            Past Papers
+          </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/q&a"
+            //rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:not-invert"
+              src="/logo.svg"
+              alt="App logo"
+              width={20}
+              height={20}
+            />
+            Question & Answer
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="account"
+          >
+            Account
           </a>
         </div>
 </header>
