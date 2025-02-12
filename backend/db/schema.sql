6cd5ba45a31c1a5fa60b6d0b9976cc26133f3db3
richyp <r@p>
2025-02-12 11:19:45 +0100

    Added db schema to track user session and interactions

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -55,5 +55,39 @@ CREATE TABLE IF NOT EXISTS course_files (
     FOREIGN KEY (fileId) REFERENCES files(id) ON DELETE CASCADE
 );
 
+-- Tables related to tracking user interactions / sessions
+-- sessions table to track user sessions
+CREATE TABLE IF NOT EXISTS sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
+-- session_context table to link sessions with relevant files used in session context
+CREATE TABLE IF NOT EXISTS session_context (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id INTEGER NOT NULL,
+    file_id INTEGER NOT NULL,
+    FOREIGN KEY (session_id) REFERENCES sessions(id) ON DELETE CASCADE,
+    FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE
+);
 
+-- interactions table to log session each session interactions
+CREATE TABLE IF NOT EXISTS interactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id INTEGER NOT NULL,
+    llm_role TEXT NOT NULL CHECK(llm_role IN ('user', 'assistant')),
+    llm_text TEXT NOT NULL,
+    llm_parts TEXT,
+    interaction_text TEXT NOT NULL,
+    interaction_type TEXT NOT NULL CHECK(interaction_type IN ('summary', 'quiz', 'chat')),
+    interaction_subtype TEXT NOT NULL CHECK(interaction_subtype IN (
+        'summary_req', 'summary_ans', 
+        'quiz_request', 'quiz_questions', 'quiz_answer_user', 'quiz_answer_feedback',
+        'chat_user', 'chat_response'
+    )),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (session_id) REFERENCES sessions(id) ON DELETE CASCADE
+);
+-- End of Tables related to tracking user interactions / sessions
